a456f410335154d0dd3b9b359ea9ffaf7cb88924
mihir21 <92274883+mihirksingh21@users.noreply.github.com>
2024-02-05 19:13:50 +0530

    designed footer and completed it sucessfully.

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { Header as HeaderType, User } from '../../../../payload/payload-types'
+import { Header as HeaderType } from '../../../../payload/payload-types'
 import { useAuth } from '../../../_providers/Auth'
 import { Button } from '../../Button'
 import { CartLink } from '../../CartLink'
@@ -16,31 +16,22 @@ export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
   const { user } = useAuth()
 
   return (
-    <nav
-      className={[
-        classes.nav,
-        // fade the nav in on user load to avoid flash of content and layout shift
-        // Vercel also does this in their own website header, see https://vercel.com
-        user === undefined && classes.hide,
-      ]
-        .filter(Boolean)
-        .join(' ')}
-    >
+    <nav className={[classes.nav, user === undefined && classes.hide].filter(Boolean).join(' ')}>
       {navItems.map(({ link }, i) => {
         return <CMSLink key={i} {...link} appearance="none" />
       })}
       <CartLink />
       {user && <Link href="/account">Account</Link>}
       {!user && (
-      <Button
-        el="link"
-        href="/login"
-        label="Login"
-        appearance="primary"
-        onClick={() => (window.location.href = "/login")}
-      />
-    )}
+        <Button
+          el="link"
+          href="/login"
+          label="Login"
+          appearance="primary"
+          onClick={() => (window.location.href = '/login')}
+        />
+      )}
+      {user && <CartLink />}
     </nav>
   )
 }
-
